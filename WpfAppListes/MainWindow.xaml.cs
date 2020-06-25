7a67d3ac7a06e84defb08d30f3603a2e4aef5abd
Anas-Emmanuel FAOUR <anas-emmanuel.faour@orange.fr>
2020-06-25 15:17:05 +0200

    binding complexe

---
+++ WpfAppListes/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppListes.Model;
+using WpfAppListes.ViewModel;
+
+namespace WpfAppListes
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GestionAdherent GestionAdherent = new GestionAdherent();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //DataContext =
+        }
+
+        private void ChargerImage_Click(object sender, MouseButtonEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            //ofd.Filter = "Fichiers tous type d'image  *.png|*.bmp|*.jepg| Autres fichiers (*.*)|*.*";
+            ofd.Filter = "Fichiers images (*.png,*.bmp,*.jpeg,*.jpg)|*.png;*.bmp;*.jpeg;*.jpg|Tous les types (*.*)|*.*";
+
+
+            if (ofd.ShowDialog() == true) // le user a selectionné et a choisit OK
+            {
+                // ofd.FileName contient alors le chemin du ficher 
+                string _path = ofd.FileName; // \var : veut dire local à ce bloc --> convention
+
+                ImgPhoto.Source = new BitmapImage(new Uri(_path));
+            }
+        }
+
+        private void AjouterAdherent_Click(object sender, RoutedEventArgs e)
+        {
+            int _n;
+            Adherent a = new Adherent();
+            if (!int.TryParse(txtNumero.Text, out _n))
+            {
+                MessageBox.Show("Numéro incorrecte");
+            }
+            else
+            {
+                a.Numero = _n;
+                a.Nom = txtNom.Text;
+                a.Prenom = txtPrenom.Text;
+                a.Photo = ImgPhoto.Source.ToString();
+
+                //GestionAdherent.AjouterAdherent(a);
+
+                string _msg = (GestionAdherent.AjouterAdherent(a) > 0 ? "Ajouté !" : "Echec de l'ajout");
+                //MessageBox.Show(_msg);
+            }
+
+        }
+
+
+    }
+}
---
+++ WpfAppListes/ViewModel/GestionAdherent.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfAppListes.Model;
+
+namespace WpfAppListes.ViewModel
+{
+    class GestionAdherent
+    {
+        // Une classe de traitement
+
+
+        //public static List<Adherent> Adherents; // Une liste static sera partagée
+
+        // Au lieu de List, on va utiliser les ObservableCollection qui sont en mesure de notifier 
+        // tout changement de la liste 
+
+        //public static ObservableCollection<Adherent> Adherents = new ObservableCollection<Adherent>(); // l'ObservableCollection est une liste qui est en mesure de notifier des changements 
+
+        // Vu l'Objet Adherent va annoncer son changement, il faut qu'elle est un GET/SET et donc une propriété privée 
+
+        private static ObservableCollection<Adherent> adherents = new ObservableCollection<Adherent>();
+
+        public static ObservableCollection<Adherent> Adherents
+        {
+            get { return adherents; }
+            set { adherents = value; }
+        }
+
+        // On instancie la collection dans le constructeur
+
+        public GestionAdherent()
+        {
+            if (adherents == null)
+            {
+                adherents = new ObservableCollection<Adherent>();
+            }
+        }
+        //méthode accessible sans instanciation 
+        public static int AjouterAdherent(Adherent adherent)
+        {
+            adherents.Add(adherent);
+            return adherents.Count;
+
+        }
+        private void InitialiserList()
+        {
+            int j = (new Random().Next(1, 1002));
+            for (int i = 0; i < 10; i++)
+            {
+                Adherent a = new Adherent();
+                a.Numero = j;
+                a.Nom = "Nom " + j;
+                a.Prenom = "Prenom " + j;
+                Adherents.Add(a);
+            }
+
+        }
+    }
+}
